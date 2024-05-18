78b3163aff847e403f23cacb28c5d34b4baa9c17
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-05-18 22:17:27 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
   const apiUrl = process.env.REACT_APP_API_URL;
+  const apiCommentUrl = apiUrl + '/comment';
+  const apiVideoUrl = apiUrl + '/video';
+  console.log(apiUrl);
+  console.log(apiCommentUrl);
+  console.log(apiVideoUrl);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          API URL: {apiUrl}
-        </p>
+        
       </header>
     </div>
   );
 }
+
 
 
 export default App;
