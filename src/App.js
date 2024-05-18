af432f0549eaef30253e3dcd9311636780a212ec
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-05-18 20:53:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const apiUrl = process.env.REACT_APP_API_URL;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          API URL: {apiUrl}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
